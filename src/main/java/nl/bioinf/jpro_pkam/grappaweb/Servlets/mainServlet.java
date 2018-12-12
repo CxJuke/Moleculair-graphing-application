36ed7e9b8c500dd0dd3cee4890911e50224cc61e
JoukeProfijt <joukepro@gmail.com>
2018-12-12 09:05:22 +0100

    Created basic servlet

---
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/mainServlet.java
@@ -0,0 +1,14 @@
+package nl.bioinf.jpro_pkam.grappaweb.Servlets;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "mainServlet")
+public class mainServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
