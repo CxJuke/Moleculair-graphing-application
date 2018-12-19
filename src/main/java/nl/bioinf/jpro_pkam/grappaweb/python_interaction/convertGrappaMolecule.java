ff182f8afd328bdff2178e112d2c1b35460f64ea
pkamphuis <priscillakamphuis@msn.com>
2018-12-19 12:00:08 +0100

    Description for new function

--- src/main/java/nl/bioinf/jpro_pkam/grappaweb/python_interaction/convertGrappaMolecule.java
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/python_interaction/convertGrappaMolecule.java
@@ -1,5 +1,27 @@
 package nl.bioinf.jpro_pkam.grappaweb.python_interaction;
 
+ /*
+* setNodes & setEdges maakt object zodat de nodes & edges opgeslagen worden
+* Hierbij moeten nog kleuren toegevoegd worden per node. Dit is de vorm die nodig is:
+* G.addNodesFrom([
+    [1, {color: 'red'}],
+    [2, {color: 'green'}],
+    [3, {color: 'white'}]
+* Input is een string met allemaal nodes (object nodes, kun je gewoon gebruiken in de nieuwe functie zonder in () te zetten)
+* en de output moet een nieuwe string worden met per node een kleur toegevoegd
+* Standard colours:
+    Hydrogen = White
+    Oxygen = Red
+    Chlorine = Green
+    Nitrogen = Blue
+    Carbon = Grey
+    Sulphur = Yellow
+    Phosphorus = Orange
+    Other = Varies - mostly Dark Red/Pink/Maroon
+  */
+
+
+
 public class convertGrappaMolecule {
 
 
@@ -14,6 +36,11 @@ public class convertGrappaMolecule {
 
     public void setEdges(String arg) {
         edges = arg.replace("(", "[").replace(")", "]");
+
+    }
+
+    public String addColour(){
+        return nodes;
 
     }
 }
