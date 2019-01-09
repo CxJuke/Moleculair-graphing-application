08712fe36ebe39741f756602017ae6b75f75d3ab
JoukeProfijt <joukepro@gmail.com>
2019-01-09 10:00:23 +0100

    Completed database connector

--- src/main/java/nl/bioinf/jpro_pkam/grappaweb/Database/databaseConnector.java
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/Database/databaseConnector.java
@@ -1,16 +1,31 @@
 package nl.bioinf.jpro_pkam.grappaweb.Database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class databaseConnector {
 
-    private Connection connect() {
+    public static void create(String filename) {
+        Connection connection = new databaseConnector().connect(filename);
+
+
+        String sql = "CREATE TABLE IF NOT EXISTS molecules (\n"
+                + "name text NOT NULL, \n"
+                + "molecule text UNIQUE\n"
+                + ");";
+
+        try (
+                Statement stmt = connection.createStatement()) {
+            stmt.execute(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private Connection connect(String database) {
         Connection connection = null;
         try {
-            String url = "jdbc:sqlite:src/main/webapp/data/molecules.sqlite";
+            String url = "jdbc:sqlite:src/main/webapp/data/" + database;
 
             connection = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -20,10 +35,10 @@ public class databaseConnector {
         }
 
 
-    public void insert(String moleculeName, String Molecule) {
+    public void insert(String moleculeName, String Molecule, String database) {
     String sql = "INSERT INTO molecules(name,molecule) VALUES(?,?)";
 
-    try (Connection connection = this.connect();
+    try (Connection connection = this.connect(database);
          PreparedStatement pstmt = connection.prepareStatement(sql)) {
         pstmt.setString(1, moleculeName);
         pstmt.setString(2, Molecule);
@@ -34,12 +49,9 @@ public class databaseConnector {
     }
     }
 
-    public static void create() {
-        String sql = "CREATE TABLE IF NOT EXISTS molecules (\n"
-                + "name text NOT NULL, \n"
-                + "molecule text UNIQUE\n"
-                + ");";
+
+
+
 
 
     }
-}
--- src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/saveMoleculeServlet.java
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/saveMoleculeServlet.java
@@ -1,5 +1,7 @@
 package nl.bioinf.jpro_pkam.grappaweb.Servlets;
 
+import nl.bioinf.jpro_pkam.grappaweb.Database.databaseConnector;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,13 @@ import java.io.IOException;
 @WebServlet(name = "saveMoleculeServlet", urlPatterns = "/save.molecule")
 public class saveMoleculeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String molecule = request.getParameter("molecule");
+        String name = request.getParameter("name");
+        String database = getServletContext().getInitParameter("database");
+        databaseConnector db = new databaseConnector();
+        db.create(database);
+        db.insert(name, molecule, database);
+
 
     }
 
