e1501422d64b2b9e8b6b7b0807da502bbe2b02c7
jprofijt <jouke.profijt@outlook.com>
2019-01-15 22:54:00 +0100

    added sqlite dependency

---
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/getMoleculeServlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "getMoleculeServlet")
+public class getMoleculeServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
