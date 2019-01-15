a5560db187000cda57e1107209011e21626c5b7e
pkamphuis <priscillakamphuis@msn.com>
2019-01-15 15:06:06 +0100

    javadoc & fixed request moleculename to search database

--- src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/SearchServlet.java
+++ src/main/java/nl/bioinf/jpro_pkam/grappaweb/Servlets/SearchServlet.java
@@ -1,5 +1,10 @@
 package nl.bioinf.jpro_pkam.grappaweb.Servlets;
 
+/**
+ * This class catch the moleculename from the searchbar on the website. This molecule is saved before and can be used
+ * to make other molecules.
+ */
+
 import nl.bioinf.jpro_pkam.grappaweb.Database.databaseConnector;
 
 import javax.servlet.ServletException;
@@ -8,20 +13,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-@WebServlet(name = "SearchServlet")
+@WebServlet(name = "SearchServlet", urlPatterns = "/Search")
 public class SearchServlet extends HttpServlet {
+    /**
+     * This function get the searchstring from the website
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     *
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String Search = request.getParameter("search");
+        String Search = request.getParameter("searchstring");
         String database = getServletContext().getInitParameter("database");
         databaseConnector db = new databaseConnector();
+        ArrayList<String> results =  db.search(Search, database);
+
+        System.out.println("results = " + results);
 
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
